13cadcd6adeb6e4267d700e197ef83a155147b0e
Norbert Szorc <n.szorc@gmail.com>
2025-09-01 03:00:58 +0200

    Updated reviews

--- src/components/TestimonialsMarquee.tsx
+++ src/components/TestimonialsMarquee.tsx
@@ -3,27 +3,33 @@ import { TestimonialCard } from "./TestimonialCard";
 
 const TESTIMONIALS = [
   {
-    name: "Kinga",
+    name: "Norbert",
     content:
-      "Izabela jest wspaniałą osobą która ma podejście do każdego, chodzę do niej ponad rok i nie zamierzam przestać ;) polecam z całego serca ❤️ jeśli ktoś chce się nauczyć angielskiego, przygotować do egzaminu czy poprostu „podtrzymać” język angielski to nie ma lepszego wyboru ❤️",
+      "Zajęcia z Izą to jedno z najlepszych doświadczeń rozwojowych, jakie spotkały mnie w ostatnich latach. Od dwóch lat regularnie wspólnie szlifujemy mój angielski, budując pewność siebie i swobodę w posługiwaniu się językiem. Dzięki jej wieloletniemu pobytowi w Wielkiej Brytanii i pięknemu akcentowi, moja wymowa osiągnęła poziom, którego wcześniej nawet sobie nie wyobrażałem. Jeśli szukasz nauczycielki, która nie tylko doskonale zna angielski, ale też żyje nim na co dzień i potrafi tą pasją zarazić innych - lepiej trafić nie można.",
     img: "https://avatar.vercel.sh/jack",
   },
   {
     name: "Rafał",
     content:
-      "Gorąco polecam korepetycje u Izabeli 🙂. Każda lekcja jest prowadzona w miłej atmosferze i zależnie od potrzeb - można trzymać się sztywno tematu lub od niego odbiegać aby nauczyć się dodatkowego słownictwa. Na lekcjach nie tylko uczyłem się angielskiego ale także podejścia do osób z zagranicy czy ogólnie do języka obcego. Pozwala to przełamać barierę i operować językiem bez większych przeszkód czy obaw że popełnię błąd. 5/5",
+      "Gorąco polecam korepetycje u Izabeli 🙂 Każda lekcja jest prowadzona w miłej atmosferze i zależnie od potrzeb - można trzymać się sztywno tematu lub od niego odbiegać aby nauczyć się dodatkowego słownictwa. Na lekcjach nie tylko uczyłem się angielskiego ale także podejścia do osób z zagranicy czy ogólnie do języka obcego. Pozwala to przełamać barierę i operować językiem bez większych przeszkód czy obaw że popełnię błąd.",
     img: "https://avatar.vercel.sh/jill",
   },
   {
     name: "Laura",
     content:
       "Serdecznie polecam! Zajęcia są świetną możliwością nie tylko do zrozumienia angielskiej gramatyki, ale również do przełamania bariery językowej. Pani Izabela jest wspaniałą kobietą z ogromnym doświadczeniem oraz bardzo profesjonalnym podejściem do ucznia. Zajęcia odbywają się w bardzo przyjemnej atmosferze! :))",
-    img: "https://avatar.vercel.sh/john",
+    img: "https://avatar.vercel.sh/jane",
   },
   {
     name: "Basia",
     content:
-      "Iza jest najlepsza 👍 motywuje, uczy według potrzeb kursanta i jest wymagająca! Lekcje z nią to czysta przyjemność 😊",
+      "Iza jest najlepsza 👍 Motywuje, uczy według potrzeb kursanta i jest wymagająca! Lekcje z nią to czysta przyjemność 😊",
+    img: "https://avatar.vercel.sh/john",
+  },
+  {
+    name: "Kinga",
+    content:
+      "Izabela jest wspaniałą osobą która ma podejście do każdego, chodzę do niej ponad rok i nie zamierzam przestać ;) Polecam z całego serca ❤️ Jeśli ktoś chce się nauczyć angielskiego, przygotować do egzaminu czy poprostu „podtrzymać” język angielski to nie ma lepszego wyboru ❤️",
     img: "https://avatar.vercel.sh/jane",
   },
   {
@@ -31,6 +37,12 @@ const TESTIMONIALS = [
     content:
       "Serdecznie polecam panią Izabelę. Uczy według potrzeb ucznia, motywuje i wymaga.",
     img: "https://avatar.vercel.sh/jenny",
+  },
+  {
+    name: "Weronika",
+    content:
+      "Bardzo polecam naukę z Izą. Lekcje są przyjemne i prowadzone w miłej atmosferze. Jasno tłumaczy gramatykę, pomaga przełamać się i zacząć mówić po angielsku. Dostosowuje tematy do potrzeb ucznia. Polecam 🙂",
+    img: "https://avatar.vercel.sh/jill",
   },
 ];
 
